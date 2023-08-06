6221dbb24be10683de69246ad1d756879e7433f5
Jose A. Garcia <101532387+soulos98@users.noreply.github.com>
2023-08-05 22:26:55 -0700

    Added logic for toDo tasks (keeps track of what is on list)

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+let lst = []; // Global list keeps track of all current toDo's
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(port, () => {
   console.log(`Server running on port number: ${port}`);
 });
 
 app.get("/", (req, res) => {
-    res.send("Hello Server!");
+  res.render("index.ejs");
+});
+
+app.post("/newEntry", (req, res) => {
+    const toDoEntry = `${req.body["todo"]}`; // String
+    lst.push(toDoEntry)
+
+
+    res.render("index.ejs", {newToDoEntry: lst});
+    console.log("All went well!");
+
+
 });
