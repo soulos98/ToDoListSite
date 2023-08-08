aaa5aaa1709d63749e9ef35448fdda763d76716f
Jose A. Garcia <101532387+soulos98@users.noreply.github.com>
2023-08-08 00:37:14 -0700

    Added routes for 'work' webpage

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-let lst = []; // Global list keeps track of all current toDo's
+let TodayList = []; // Global list keeps track of all current toDo's
+let WorkList = [];
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public/")); // Sets up the location of our css files
@@ -14,14 +15,26 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  TodayList.length > 0
+    ? res.render("index.ejs", { newToDoEntry: TodayList })
+    : res.render("index.ejs");
 });
 
 app.post("/newEntry", (req, res) => {
-    const toDoEntry = `${req.body["todo"]}`; // User entry
-    lst.push(toDoEntry) // Append new toDo item to global lst
+  const toDoEntry = `${req.body["todo"]}`; // User entry
+  TodayList.push(toDoEntry); // Append new toDo item to global TodayList
 
+  res.render("index.ejs", { newToDoEntry: TodayList });
+  console.log("All went well!");
+});
 
-    res.render("index.ejs", {newToDoEntry: lst});
-    console.log("All went well!");
+app.get("/work", (req, res) => {
+  WorkList.length > 0 ? res.render("work.ejs", {newToDoEntry: WorkList}) : res.render("work.ejs");
+});
+
+app.post("/newWorkEntry", (req, res) => {
+  const workEntry = `${req.body["todo"]}`;
+  WorkList.push(workEntry);
+
+  res.render("work.ejs", { newToDoEntry: WorkList });
 });
